8d894391b7830a36bf27641c7308c8de755d052d
matteo <matteodragonfire2312@gmail.com>
2024-05-18 19:00:26 +0200

    i'm alredy stuck

---
+++ commands/help.go
@@ -0,0 +1,9 @@
+package commands
+
+import "fmt"
+
+func callHelp() error {
+	fmt.Println("help message")
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/matteomello/commands"
+)
+
+type cliCommand struct {
+	command     string
+	description string
+	callBack    func() error
+}
+
+var commandMap = map[string]cliCommand{
+
+	"help": {
+
+		command:     "help",
+		description: "display a help message",
+		callBack:    commands.callHelp,
+	},
+
+	"exit": {
+
+		command:     "exit",
+		description: "exit the program",
+		callBack:    commands.callExit,
+	},
+}
 
 func main() {
 
-	fmt.Print("ciao")
 }
